8cf457e91de643f57cdd373dbb0c5eefeb508e85
Munir Mahmud <munirmahmud6@gmail.com>
2020-11-19 16:58:46 +0600

    Displayed messages to the user

--- src/components/Messages/MessageContent/MessageContent.js
+++ src/components/Messages/MessageContent/MessageContent.js
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MessageContent.styles.css';
+import firebase from "../../../server/firebase";
+import {connect} from "react-redux";
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+
+TimeAgo.locale(en);
+const timeAgo = new TimeAgo();
 
 const MessageContent = (props) => {
+    const messagesRef = firebase.database().ref('messages');
+    const [messages, setMessages] = useState([]);
 
+    useEffect(() => {
+        if (props.channel) {
+            messagesRef.child(props.channel.id).on('child_added', snap => {
+                setMessages(currentState => {
+                    let updatedState = [...currentState];
+                    updatedState.push(snap.val());
+                    return updatedState;
+                });
+            });
+
+            return () => messagesRef.off();
+        }
+    }, [props.channel]);
+    
     return (
         <div className="message__content">
-            <div className="row">
-                <div className="message__content-wrapper">
-                    <img src="https://ca.slack-edge.com/T04GZKGQY-U014TDGHJQN-fe8d05f98df9-48" alt="Avatar"/>
-                    <div className="message__info">
-                        <strong>Munir Mahmud <span>Time...</span></strong>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi aspernatur debitis deserunt eligendi eum exercitationem, illum laudantium nisi officia officiis sint, unde vero vitae. Architecto at delectus exercitationem ipsa veritatis!</p>
+            {messages.length > 0 && messages.map(message => (
+                <div key={message.timestamp} className="row">
+                    <div className="message__content-wrapper">
+                        <img src={message.user.avatar} alt={message.user.name} />
+                        <div className="message__info">
+                            <strong>{message.user.name} <span>{timeAgo.format(message.timestamp)}</span></strong>
+                            <p>{message.content}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 };
 
-export default MessageContent;
+const mapStateToProps = (state) => {
+    return {
+        channel: state.channel.currentChannel
+    }
+};
+
+export default connect(mapStateToProps) (MessageContent);
